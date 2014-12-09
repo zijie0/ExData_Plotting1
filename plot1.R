fa35e4edf3c4a204930a991b65ddfc764cbd7d09
zijie0 <zijie.zy@alibaba-inc.com>
2014-12-09 19:52:08 +0800

    Final commit

--- plot1.R
+++ plot1.R
@@ -1,12 +1,11 @@
 # Read data
 data <- read.csv("./data/household_power_consumption.txt", header=T, 
                  sep=';', na.strings="?", nrows=70000, check.names=F, 
-                 stringsAsFactors=F, comment.char="", quote='\"')
+                 stringsAsFactors=F)
 data$Date <- as.Date(data$Date, format="%d/%m/%Y")
 
 # Subset the data
 sub_data <- subset(data, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
-rm(data)
 
 # Draw Plot 1
 par(cex.lab=0.9, cex.main=0.9, cex.axis=0.9)
--- plot4.R
+++ plot4.R
@@ -1,15 +1,14 @@
 # Read data
 data <- read.csv("./data/household_power_consumption.txt", header=T, 
                  sep=';', na.strings="?", nrows=70000, check.names=F, 
-                 stringsAsFactors=F, comment.char="", quote='\"')
+                 stringsAsFactors=F)
 data$Date <- as.Date(data$Date, format="%d/%m/%Y")
 
 # Subset the data
 sub_data <- subset(data, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
-rm(data)
 
 # Convert dates
-datetime <- paste(as.Date(sub_data$Date), sub_data$Time)
+datetime <- paste(sub_data$Date, sub_data$Time)
 sub_data$Datetime <- as.POSIXct(datetime)
 
 # Draw Plot4
